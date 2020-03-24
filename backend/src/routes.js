326a73bf7b56d063185ad918c1a79e2c4ec79ccf
joaoayalla <joaoayalla19@gmail.com>
2020-03-24 18:49:57 -0300

    Dia 2 finalizado :)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const OngController = require("./controllers/OngController");
 const IncidentController = require("./controllers/IncidentController");
+const ProfileController = require("./controllers/ProfileController");
+const SessionController = require("./controllers/SessionController");
 const routes = express.Router();
+
+// login
+routes.post("/sessions", SessionController.create);
 
 // listagem de ongs
 routes.get("/ongs", OngController.index);
 
 // cadastro de ongs
 routes.post("/ongs", OngController.create);
+
+// listar uma ong especifica
+routes.get("/profile", ProfileController.index);
 
 // listagem de incidents
 routes.get("/incidents", IncidentController.index);
